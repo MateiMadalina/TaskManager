2e5d83e547e41e06cb21a216d33afdc45a1f7ed2
MateiMadalina <116349352+MateiMadalina@users.noreply.github.com>
2023-11-27 19:32:03 +0200

    design a part of nav bar

--- frontend/task-manager/src/App.js
+++ frontend/task-manager/src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
+import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider } from "react-router-dom";
+import RootLayout from "./layout/RootLayout";
+import Home from "./pages/Home";
+import Tasks from "./pages/Tasks";
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<RootLayout/>}>
+          <Route index element={<Home/>}/>
+          <Route path="/tasks" element={<Tasks/>}/>
+        </Route>
+    )
+)
+
+export default function App() {
+  return <RouterProvider router={router}/>
 }
-
-export default App;
---
+++ frontend/task-manager/src/layout/RootLayout.jsx
@@ -0,0 +1,39 @@
+import {NavLink, Outlet} from "react-router-dom";
+import Logo from "../components/img/logo.png"
+
+const RootLayout = () => {
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg  sticky-top text-white" data-bs-theme="dark">
+                <div className="container-fluid">
+                    <NavLink to="/">
+                        <img height={65} className="ps-2" src={Logo} alt="logo"/>
+                    </NavLink>
+                    <div className="collapse navbar-collapse" id="navbarText">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <NavLink to="/tasks" className="nav-link text-black">Tasks</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/login" className="btn btn-outline-dark me-2">Log in</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/signup" className="btn btn-dark text-white me-2">Sign up</NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <div className="root-layout" style={{minHeight: "91.5vh"}}>
+                <header>
+                </header>
+                <main id="main" className="">
+                    <Outlet/>
+                </main>
+            </div>
+        </>
+
+    );
+}
+
+export default RootLayout;
